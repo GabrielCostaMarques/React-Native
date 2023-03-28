6befba70d0e5da3f5e2c8191722bb34d5d39394f
GabrielCostaMarques <gabrielcostamarques2@gmail.com>
2023-03-28 16:59:12 -0300

    aplicando um formulario solo

--- exePet/App.js
+++ exePet/App.js
@@ -1,72 +1,50 @@
 import React from "react";
-import { ScrollView } from "react-native";
-import { TextInput } from "react-native";
-import { Button, View, Text, ImageBackground } from "react-native";
+import { Text, View, Button, ImageBackground, TextInput } from "react-native";
 
-import imgCheck from './assets/check.jpg';
+import ImgCarro from "./assets/car-sale.jpg";
 
-class CheckpointFomulario extends React.Component{
+class FormularioCarro extends React.Component{
     constructor(props){
-        super(props);
-        this.state={materia:"Hybrid",numCheck:"#1",nota:"5,0", lista:[]} //lista foi declarada para guardar os objetos digitados no textinput nela
+        super(props)
+        this.state={modelo:"", marca:"",preco:"",lista:[]}
     }
-
-
     render(){
-    //Vinculando o State com os textInput
-    // OnChangeText, esse atributo serve para atualizar o textInput sempre que o usuario digitar algo
-    // o parâmetro txt vai receber oq for atualizado no OnChangeText, por isso ele precisa ser passado
-    
-    const listaVisual=[]
-    
-    for (let i = 0; i < this.state.lista.length; i++) { //for para percorrer os objetos da lista
-        const obj=this.state.lista[i]; //passando a lista de objetos para uma variável
-        const elementoVisual= //criando uma variável para a view da lista que vai aparecer
-
-        //a key serve para cada elemento printado ter uma chave unica, entao o "i" sempre vai aumentar de um em um por causa do for
-        <View key={"key"+i} style={{backgroundColor:"green"}}> 
-            <Text>Materia: {obj.materia}</Text>
-            <Text>Checkpoint #: {obj.numCheck}</Text>
-            <Text>Nota: {obj.nota}</Text>
-        </View>;
-
-        listaVisual.push(elementoVisual);
-
-            };
-        return( 
-            <View style={{flex:1}}>
-                <Text>Nome da matéria</Text>
-                <TextInput value={this.state.materia} onChangeText={(txt)=>{
-                    this.setState({materia:txt})
-                }}/> 
-                <Text>Número do CheckPoint</Text>
-                <TextInput value={this.state.numCheck} onChangeText={(txt)=>{
-                    this.setState({numCheck:txt})
+        return(
+            <View>
+                <Text>Modelo do carro</Text>
+                <TextInput placeholder="Digite o modelo" value={this.state.modelo}onChangeText={(txt)=>{
+                    this.setState({modelo:txt})
                 }}/>
-                <Text>Nota Obtida</Text>
-                <TextInput value={this.state.nota}onChangeText={(txt)=>{
-                    this.setState({nota:txt})
-                }}/> 
+                <Text>Marca do carro</Text>
+                <TextInput placeholder="Digite a marca" value={this.state.marca} onChangeText={(txt)=>{
+                    this.setState({marca:txt})
+                }}></TextInput>
+                <Text>Preço do Carro</Text>
+                <TextInput placeholder="Digite o preço" value={this.state.preco} onChangeText={(txt)=>{
+                    this.setState({preco:txt})
+                }}></TextInput>
+
                 <Button title="Salvar" onPress={()=>{
-                    const obj={ materia: this.state.materia, 
-                                numCheck: this.state.numCheck, 
-                                nota: this.state.nota};
-                    this.setState({lista:[...this.state.lista,obj]}) //tres pontos serve para a lista nao apagar os outros dados que foram inseridos na lista
-                }}/>
-                <ScrollView style={{flex:1}}>{listaVisual}</ScrollView>
-                
-            </View>//imprimindo a lista com o conteudo que foi criado
-
+                    const obj={modelo:this.state.modelo,
+                                marca: this.state.marca,
+                                preco: this.state.preco}
+                    this.setState({...this.state.lista,obj})
+                }}></Button>
+            </View>
         )
     }
 }
 
+
 export default ()=>
     <View style={{flex:1}}>
-        <ImageBackground style={{flex:1}} source={imgCheck} resizeMode="cover">
 
-        </ImageBackground>
         <View style={{flex:1}}>
-        <CheckpointFomulario/>
+            <ImageBackground style={{flex:1}}source={ImgCarro} resizeMode="cover"></ImageBackground>
         </View>
+        <Text>Formulario de Carros</Text>
+        <View style={{flex:1}}>
+        <FormularioCarro/>
+        </View> 
     </View>
+    
