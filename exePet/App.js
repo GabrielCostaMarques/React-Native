97f87ddef2133a8f4c068b1b095e55da082240de
GabrielCostaMarques <gabrielcostamarques2@gmail.com>
2023-05-23 01:52:17 -0300

    prova

--- exePet/App.js
+++ exePet/App.js
@@ -1,52 +1,91 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import React,{useState} from 'react';
+import { Button,Text,TextInput,View } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
-  const [numero, setNumero] = useState(0);
+const {Navigator, Screen}=createBottomTabNavigator();
 
-  const incrementar = () => {
-    setNumero(numero + 1);
+const login=(user,pass)=>{
+    AsyncStorage.getItem("USUARIO")
+    .then((inf)=>{
+      const o = JSON.parse(inf);
+      if (dados['email']==user && dados['senha']==pass){
+        alert("Usuário logado com Sucesso")
+      }else{
+        alert("Usuário ou senha incorretos");
+      }
+    })
+    .catch(()=>{})
+    let a =2;
   }
 
-  const decrementar = () => {
-    setNumero(numero - 1);
-  }
+const Login=()=>{
+  const [email, setEmail] = useState("")
+  const [senha, setSenha] = useState("")
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>CONTADOR: {numero}</Text>
-      <TouchableOpacity style={styles.button} onPress={incrementar}>
-        <Text style={styles.buttonText}>+</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={decrementar}>
-        <Text style={styles.buttonText}>-</Text>
-      </TouchableOpacity>
+  return(
+    <View>
+      <Text>Java Café</Text>
+      <Text>Bebidas</Text>
+      <Text>Email</Text>
+      <TextInput value={email} onChangeText={setEmail}/>
+      <Text>Password</Text>
+      <TextInput value={senha} onChangeText={setSenha}/>
+      <Button title='Login' onPress={()=>{login(email, senha)}}/>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor:'#000'
-  },
-  text: {
-    color:'white',
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 24,
-  },
-  button: {
-    backgroundColor: '#fff',
-    padding: 25,
-    borderRadius: 8,
-    margin: 12,
-  },
-  buttonText: {
-    color: '#000',
-    fontSize: 40,
-    fontWeight: 'bold',
-  },
-});
+const Cadastro =(props)=>{
+
+  return(
+    <View style={{flex:1}}>
+      <Text>Cadastro</Text>
+      <Text>Nome da Bebida</Text>
+      <TextInput/>
+      <Text>Tamanho da bebida</Text>
+      <TextInput/>
+      <Text>Preço</Text>
+      <TextInput/>
+  </View>
+
+)
+
+}
+const Listagem =(props)=>
+<View style={{flex:1}}>
+  <Text>
+    Listagem
+  </Text>
+</View>
+export default function App(){
+  const [lista, setLista]=useState([])
+
+  const inserir =()=>{
+
+  }
+  return(
+    <NavigationContainer>    
+        <View style={{flex:1}}>
+          <View>
+            <Text>Java cafe e bebidas</Text>
+          </View>
+          <View style={{flex:1}}>
+            <Navigator>
+              <Screen name='Cadastro'>
+                {()=><Cadastro onInserir={inserir}/>}
+              </Screen>
+              
+              <Screen name='Listagem'>
+                {()=><Listagem lista={lista}/>}
+
+              </Screen>
+
+            </Navigator>
+          </View>
+      </View>
+    </NavigationContainer>
+
+  )
+}
