4d8e2059c5a33495b7e976521a24248faec97efd
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-18 21:32:35 -0300

    Tab navigator

--- Atividade30/Teste1/App.js
+++ Atividade30/Teste1/App.js
@@ -1,12 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, ImageBackground, Button } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  ImageBackground,
+  Button,
+} from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import ImgMain from './assets/WalpaperMain.png'
-
+import ImgMain from "./assets/WalpaperMain.png";
+import { NavigationContainer } from "@react-navigation/native";
 
 const Login = () => {
   return (
     <View style={estilos.container}>
+      <View style={{ flex: 1 }}>
+        <ImageBackground
+          style={estilos.imagemCafe}
+          source={ImgMain}
+        ></ImageBackground>
+      </View>
       <View style={estilos.login}>
         <Text style={estilos.textoLogin}>Login</Text>
         <Text>Preencha seus dados para continuar</Text>
@@ -14,32 +29,33 @@ const Login = () => {
 
       <View>
         <Text style={estilos.textoDefinicao}>EMAIL</Text>
-        <TextInput
-          style={estilos.input}
-          placeholder="Digite seu E-mail" />
+        <TextInput style={estilos.input} placeholder="Digite seu E-mail" />
 
         <Text style={estilos.textoDefinicao}>PASSWORD</Text>
 
         <TextInput
           style={estilos.input}
           secureTextEntry={true}
-          placeholder="Digite sua senha" />
+          placeholder="Digite sua senha"
+        />
       </View>
 
       <View styel={estilos.cadastro}>
-        <TouchableOpacity onPressIn={() => { alert("Seus dados foram cadastrados!") }}>
-
+        <TouchableOpacity
+          onPressIn={() => {
+            alert("Seus dados foram cadastrados!");
+          }}
+        >
           <View>
             <Text style={estilos.botaoCadastrar}>Cadastrar</Text>
           </View>
-
         </TouchableOpacity>
 
         <Text style={estilos.signUp}>Signup!</Text>
       </View>
     </View>
-  )
-}
+  );
+};
 
 const Cadastro = () => {
   return (
@@ -48,102 +64,92 @@ const Cadastro = () => {
         <Text>Detalhes do Restaurante</Text>
       </View>
       <View>
-        <TextInput placeholder='Nome do Restaurante' />
-        <TextInput placeholder='Endereço' />
-        <TextInput placeholder='Tipo de Comida' />
+        <TextInput placeholder="Nome do Restaurante" />
+        <TextInput placeholder="Endereço" />
+        <TextInput placeholder="Tipo de Comida" />
       </View>
       <View>
         <Text>Classificação</Text>
       </View>
       <View>
-        <TextInput placeholder='Latitude' />
-        <TextInput placeholder='Longitude' />
+        <TextInput placeholder="Latitude" />
+        <TextInput placeholder="Longitude" />
       </View>
       <View>
-        <TextInput placeholder='Descrição' />
+        <TextInput placeholder="Descrição" />
 
-        <TouchableOpacity onPressIn={() => { alert("Seus dados foram cadastrados!") }}>
+        <TouchableOpacity
+          onPressIn={() => {
+            alert("Seus dados foram cadastrados!");
+          }}
+        >
           <View>
             <Text style={estilos.botaoCadastrar}>Salvar</Text>
           </View>
         </TouchableOpacity>
       </View>
     </View>
-  )
-}
+  );
+};
 
-
+const Tab = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={{ flex: 1 }}>
-      <View style={{ flex: 1 }}>
-        <ImageBackground
-          style={estilos.imagemCafe}
-          source={ImgMain}>
-        </ImageBackground>
-      </View>
-      <View>
-        <Login />
-      </View>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Login} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
-
-
-
 const estilos = StyleSheet.create({
-
   container: {
     backgroundColor: "#efe4e1",
     flex: 2,
     justifyContent: "center",
-
   },
-
 
   imagemCafe: {
     flex: 3,
     height: 200,
-    width: 500
-
+    width: 500,
   },
 
   titulo: {
     fontSize: 40,
     justifyContent: "center",
-    backgroundColor: 'rgba(116, 112, 112, 0.4)',
+    backgroundColor: "rgba(116, 112, 112, 0.4)",
     borderRadius: 25,
     fontWeight: "bold",
     textAlign: "center",
     padding: 10,
     margin: 40,
-    marginHorizontal: 20
+    marginHorizontal: 20,
   },
 
   login: {
     flex: 7,
     justifyContent: "center",
     alignItems: "center",
-    marginBottom: 10
+    marginBottom: 10,
   },
 
   cadastro: {
-    margin: 40
+    margin: 40,
   },
 
   textoLogin: {
     fontSize: 50,
     fontWeight: "bold",
-    marginBottom: 8
-
+    marginBottom: 8,
   },
 
   textoDefinicao: {
     marginBottom: 5,
     marginLeft: "15%",
-    marginRight: "15%"
+    marginRight: "15%",
   },
 
   input: {
@@ -153,7 +159,7 @@ const estilos = StyleSheet.create({
     marginBottom: 10,
     paddingHorizontal: 20,
     marginLeft: "14%",
-    marginRight: "14%"
+    marginRight: "14%",
   },
 
   botaoCadastrar: {
@@ -167,11 +173,11 @@ const estilos = StyleSheet.create({
     marginTop: 20,
     paddingHorizontal: 20,
     marginLeft: "14%",
-    marginRight: "14%"
+    marginRight: "14%",
   },
 
   signUp: {
     textAlign: "center",
-    marginTop: 50
-  }
+    marginTop: 50,
+  },
 });
